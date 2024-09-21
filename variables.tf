6aaef2f6931d06417904df765c8968cbc6e63816
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2024-09-21 13:12:18 +0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "instance_name1" {
+  type  = string  
+  default  = "Window-VSCODE/XAMP"
+}
+
+variable "instance_name2" {
+  type  = string  
+  default  = "Window-HADOOP"
+}
+
+variable "keypair_name" {
+  type = string
+  default = "ssh_key"
+}
