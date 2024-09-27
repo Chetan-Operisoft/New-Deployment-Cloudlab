4b80c30ebed0bbe36ec67647ec83295ad902d286
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2024-09-27 11:05:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,15 +6,14 @@ provider "aws" {
 resource "aws_security_group" "master" {
   vpc_id = "vpc-0f28bcc0b6a596b84"
 
-  # port 22 for ssh connection
+# port 22 for ssh conection
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # port 3389 for RDP connection
+# port 3306 for db connection
   ingress {
     from_port   = 3389
     to_port     = 3389
@@ -22,12 +21,12 @@ resource "aws_security_group" "master" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # open to all
+# open to all
   ingress {
     from_port = 0
-    to_port   = 0
-    protocol  = -1
-    self      = true
+    to_port = 0
+    protocol = -1
+    self = true
   }
 
   egress {
@@ -38,18 +37,16 @@ resource "aws_security_group" "master" {
   }
 }
 
-
 resource "tls_private_key" "master-key-gen" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Create the Key Pair of kali linux didnt have software
+# Create the Key Pair of Kali Linux didn't have software
 resource "aws_key_pair" "master-key-pair" {
   key_name   = var.keypair_name 
   public_key = tls_private_key.master-key-gen.public_key_openssh
 }
-
 # Exploitable Windows - VSCODE_XAMP
 resource "aws_instance" "Window_VSCODE_XAMP" {
   ami           = "ami-0e531e2365203ce30"  # Replace with your desired AMI ID
