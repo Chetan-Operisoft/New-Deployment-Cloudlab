f79681c73c4e81467aa7b77c46552900d03be975
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2024-09-27 11:13:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
+variable "keypair_name" {
+  type        = string
+  description = "Name of the key pair"
+}
+
 variable "instance_name1" {
-  type  = string  
-  default  = "Window-VSCODE/XAMP"
+  type        = string
+  description = "Name of the EC2 instance"
 }
