075c2db8bb50a05fe20dc0129139cfa91fe2519c
Chetan-Operisoft <128120269+Chetan-Operisoft@users.noreply.github.com>
2024-09-27 11:03:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,12 +38,13 @@ resource "aws_security_group" "master" {
   }
 }
 
+
 resource "tls_private_key" "master-key-gen" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Create the Key Pair of Kali Linux didn't have software
+# Create the Key Pair of kali linux didnt have software
 resource "aws_key_pair" "master-key-pair" {
   key_name   = var.keypair_name 
   public_key = tls_private_key.master-key-gen.public_key_openssh
